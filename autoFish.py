7f081dc85bb9489f959585a43901b5224065d21e
lbw <li.bowen3@iwhalecloud.com>
2024-10-07 12:52:15 +0800

    优化

--- autoFish.py
+++ autoFish.py
@@ -9,17 +9,18 @@ import pyautogui
 
 user32 = ctypes.WinDLL('user32', use_last_error=True)
 gdi32 = ctypes.WinDLL('gdi32', use_last_error=True)
+hdc = user32.GetDC(None)
 
 
 def get_color(x, y):
     # gdi32 = windll.gdi32
     # user32 = windll.user32
-    hdc = user32.GetDC(None)  # 获取颜色值
+#     hdc = user32.GetDC(None)  # 获取颜色值
     pixel = gdi32.GetPixel(hdc, x, y)  # 提取RGB值
     r = pixel & 0x0000ff
     g = (pixel & 0x00ff00) >> 8
     b = pixel >> 16
-    user32.ReleaseDC(None, hdc)
+#     user32.ReleaseDC(None, hdc)
     return [r, g, b]
 
 
